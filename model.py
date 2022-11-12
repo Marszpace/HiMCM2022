a01f0dcef3f8e9d119da0cb58502311b39f5582a
Marszpace <66119805+Marszpace@users.noreply.github.com>
2022-11-12 22:38:31 +0700

    comment+load data
    wrote comment and finished load data function

--- model.py
+++ model.py
@@ -14,46 +14,54 @@ def seasonal(Xn,time,data,coefficient):
 class model:
     def load_data(self):
         #data=["example.csv","example2.csv"]
+        #main="data.csv"
         data=[]
-        n=0
+
+        #load actual data
+        df=pd.read_csv(i)
+        self.actual=df.values.tolist()
         
+        #load parameter data
+        n=0
         for i in data:
             df=pd.read_csv(i)
             self.data.append(df.values.tolist())
             self.coefficient.append(0)
 
     def predict(self,time):
+        #some dp
         if(time==0):
             return self.X[0]
         if(time!=len(self.X)):
-            XPrev = self.predict(time-1,k)
+            XPrev = self.predict(time-1,self.static[0],self.static[1])
         else:
             XPrev = self.X[time-1]
+        #prediction
         XStatic = static(XPrev,self.static[0],self.static[1])
         XSeasonal = seasonal(XPrev,time,self.data,self.coefficient)
+        #save data
         self.X.append(XStatic+XSeasonal)
+        #return
         return XStatic+XSeasonal
 
     def train(self,period,time):
         #Only this part is left
         pass
 
-    def __init__(self,x0,k,rmax):
+    def __init__(self,x0):
+        #modify initial parameters here
         self.X=[x0]
-        self.static=[k,rmax]
+        self.static=[1500,0.15] #k,rmax
         self.data=[]
         self.coefficient=[]
         self.actual=[]
 
-k=1500
-rmax=0.15
-arstneio = model(0.03,k,rmax)
+
+arstneio = model(0.03)
 arst=[]
 
 for i in range(100):
-    #print(arstneio.predict(i,k))
     arst.append(arstneio.predict(i))
-    #k-=10
 
 plt.plot(arst)
 plt.show()
